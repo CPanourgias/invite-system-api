50e6aee6b59d35aeac82ddc4a7baec9e11bb0f1e
CPanourgias <panourgias.christos@gmail.com>
2019-09-01 23:04:54 +0300

    Added SenderController tests

---
+++ tests/Controller/SenderControllerTests.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RecieverControllerTest extends WebTestCase
+{
+    public function testRecieverController()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/sender/1/view');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+
+        $client->request('GET', '/reciever/1/send_1');
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+
+        $client->request('GET', '/reciever/1/cancel_1');
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+    }
+
+}
